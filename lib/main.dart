b6bc6a47f6f9f8f4369e9804d2ed7b8a5b4401c2
elson <elsonnezaj2005@gmail.com>
2024-02-25 15:46:04 +0100

    learned about Container, Box Decoration, Text, Text Styles and Center, Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,26 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+  runApp(
+    MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('First App'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Text(
-                'Hello World!',
+        body: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(colors: [
+            Color.fromARGB(255, 91, 44, 173),
+            Color.fromARGB(255, 55, 17, 121)
+          ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+          child: const Center(
+            child: Text(
+              "Hello World",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 32,
               ),
-              Text(
-                'It\'s time to learn Flutter!',
-              ),
-            ],
+            ),
           ),
         ),
       ),
-    );
-  }
+    ),
+  );
 }
