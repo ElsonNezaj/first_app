35bd89a5cf9dd3d732d5f274141353b027bc6bb4
elson <elsonnezaj2005@gmail.com>
2024-02-28 11:30:52 +0100

    created custom widget for text

---
+++ lib/gradient_container.dart
@@ -0,0 +1,24 @@
+import "package:flutter/material.dart";
+import "package:first_app/styled_text.dart";
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 91, 44, 173),
+            Color.fromARGB(255, 55, 17, 121)
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: StyledText(),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import 'package:first_app/gradient_container.dart';
 
 void main() {
   runApp(
@@ -6,32 +7,4 @@ void main() {
       home: Scaffold(body: GradientContainer()),
     ),
   );
-}
-
-class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
-  @override
-  Widget build(context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 91, 44, 173),
-            Color.fromARGB(255, 55, 17, 121)
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
-      ),
-      child: const Center(
-        child: Text(
-          "Hello World",
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 32,
-          ),
-        ),
-      ),
-    );
-  }
 }
